ad889202cb2db8dd1c9714ca48456d5d24df6529
Vaibhav Bhavsar <Vaibhavbhavsar007@gmail.com>
2019-11-09 21:54:33 +0530

    key changed

--- setup/myurl.js
+++ setup/myurl.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURL: "mongodb://vaibhav:vaibhav123@ds247001.mlab.com:47001/bigstackproject",
+    mongoURL: "mongodb://vaibhav:qwertyuiop@ds247001.mlab.com:47001/bigstackproject",
     secret: "mystrongsecret"
   };
 
